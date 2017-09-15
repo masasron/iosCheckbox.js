561948252ba8026504c9b43b7fac941db1c1b5bd
Ron Masas <ronm@bbinary.com>
2017-09-15 15:20:37 +0300

    Support disabled and onclick events.

--- iosCheckbox.js
+++ iosCheckbox.js
@@ -11,10 +11,13 @@
             		$(this).next('.ios-ui-select').remove();
                 });
             };
+            
             if ($(this).attr('data-ios-checkbox') === 'true') {
                 return;
             }
+            
             $(this).attr('data-ios-checkbox', 'true');
+            
             $(this).each(function() {
                 /**
                  * Original checkbox element
@@ -33,8 +36,15 @@
                 if (org_checkbox.is(":checked")) {
                     ios_checkbox.addClass("checked");
                 }
+                
                 // Hide the original checkbox and print the new one.
                 org_checkbox.hide().after(ios_checkbox);
+                
+                if (org_checkbox.is(":disabled")){
+                   // In case the original checkbox is disabled don't register the click event.
+                	 return ios_checkbox.css('opacity','0.6');
+                }
+                
                 // Add click event listener to the ios checkbox
                 ios_checkbox.click(function() {
                     // Toggel the check state
@@ -47,6 +57,9 @@
                         // Update state
                         org_checkbox.prop('checked', false);
                     }
+                    
+                    // Run click even in case it was registered to the original checkbox element.
+                	org_checkbox.click();
                 });
             });
             return this;
